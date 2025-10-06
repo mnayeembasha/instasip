8c0bebce7dad42a8180210bf36e0f1966a816d8e
mnayeembasha <mnayeembasha@gmail.com>
2025-10-06 15:35:55 +0530

    updated api url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export const DOMAIN = "https://instasip.vercel.app";
 
-export const BACKEND_URL = import.meta.env.MODE === "development" ? "http://localhost:8080/api": "/api";
+export const BACKEND_URL = import.meta.env.MODE === "development" ? "http://localhost:8080/api": "https://instasip-server.onrender.com/api";
 
