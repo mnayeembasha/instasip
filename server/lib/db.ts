671c3cbdf18c9cf7c9546aae7cfd522dd8edbb40
mnayeembasha <mnayeembasha@gmail.com>
2025-10-12 22:06:16 +0530

    implemented singleton pattern in db connection

--- server/lib/db.ts
+++ server/lib/db.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
 import { MONGODB_URI } from '../config';
+let isConnected = false;
 
 export const connectDB = async (): Promise<void> => {
+    if (isConnected) return;
     try {
         await mongoose.connect(MONGODB_URI!);
         console.log(`MongoDB connected`);
+         isConnected = true;
     } catch (error) {
         console.error(`Database connection error: ${error}`);
         process.exit(1);
