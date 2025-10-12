fcdbbbf1f5ec21b2c3a5e0b889df7cf5196b1fa6
mnayeembasha <mnayeembasha@gmail.com>
2025-10-12 09:11:27 +0530

    add posthog

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App';
 import './index.css';
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+   <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
     <HelmetProvider>
       <App />
     </HelmetProvider>
+   </PostHogProvider>
   </React.StrictMode>
 );
 
