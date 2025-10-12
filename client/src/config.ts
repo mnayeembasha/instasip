6ed175d0bedf51ba6340d8104a406140556f11f6
mnayeembasha <mnayeembasha@gmail.com>
2025-10-12 17:45:41 +0530

    fix: modified ci to push node_modules to lambda

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export const DOMAIN = "https://instasip.in";
 
-export const BACKEND_URL = import.meta.env.MODE === "development" ? "http://localhost:8080/api": "https://instasip-server.onrender.com/api";
+export const BACKEND_URL = import.meta.env.MODE === "development" ? "https://umdjyisiqi.execute-api.ap-south-1.amazonaws.com/prod/api/": "https://instasip-server.onrender.com/api";
 
