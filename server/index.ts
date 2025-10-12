5a6d09c6cdd8314c55c5e8a032563e06c68c2b17
mnayeembasha <mnayeembasha@gmail.com>
2025-10-12 21:19:31 +0530

    add a health check endpoint

--- server/index.ts
+++ server/index.ts
@@ -41,6 +41,10 @@ app.use(express.json({ limit: '7mb' }));
 app.use(express.urlencoded({ extended: true, limit: '7mb' }));
 app.use(cookieParser());
 
+app.get("/api/health",(req:Request,res:Response)=>{
+  res.status(200).json({status:"OK",message:"Hello from Instasip API"});
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use('/api/cart', cartRoutes);
