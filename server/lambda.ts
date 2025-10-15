f7152656ca046e5238403f8590ea68e69f0cb576
mnayeembasha <mnayeembasha@gmail.com>
2025-10-15 20:14:23 +0530

    fix:lambda handler export issue

--- server/lambda.ts
+++ server/lambda.ts
@@ -1,17 +1,17 @@
-// import serverless from "serverless-http";
-// import app from "./index";  
-// import { connectDB } from "./lib/db";
+import serverless from "serverless-http";
+import app from "./index";  
+import { connectDB } from "./lib/db";
 
-// export const handler = serverless(app);
-// export const lambdaHandler = async (event: any, context: any) => {
-//   try {
-//     await connectDB();
-//     return await handler(event, context);
-//   } catch (err) {
-//     console.error("Lambda caught error:", err);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ message: "Internal Server Error" }),
-//     };
-//   }
-// };
+export const handler = serverless(app);
+export const lambdaHandler = async (event: any, context: any) => {
+  try {
+    await connectDB();
+    return await handler(event, context);
+  } catch (err) {
+    console.error("Lambda caught error:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
